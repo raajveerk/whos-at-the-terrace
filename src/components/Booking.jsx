b6119a360f9dc6cdd7cf91711a7f10dd26ecc0f8
raajveerk <khatarraajveer@gmail.com>
2023-02-14 22:39:09 +0530

    minor UI changes

--- src/components/Booking.jsx
+++ src/components/Booking.jsx
@@ -1,39 +1,107 @@
 import React, { useState } from 'react'
 import { CiBeerMugFull } from "react-icons/ci"
 import { GiPartyPopper } from "react-icons/gi"
+// import { CSSTransition } from 'react-transition-group'
+import "./Booking.scss";
 // import { DatePicker } from "react-date-picker/dist/DatePicker";
 // import { Moment } from "moment";
 
 export const Booking = () => {
 
-    const [visible, setVisible] = React.useState(false);
+    const [visible, setVisible] = useState(false);
 
-    const [date, setDate] = useState();
+    // let datePickStyleUno = {
+    //     display: visible ? 'flex' : 'none',
+    //     opacity: '0'
+    // }
+
+    // let datePickStyleDos = {
+    //     display: visible ? 'flex' : 'none',
+    //     opacity: '1',
+    //     transition: 'opacity 1.2s'
+    // }
+
+    // let bookBtnStyleUno = {
+    //     display: visible ? 'flex' : 'none',
+    //     opacity: '0'
+    // }
+
+    // let bookBtnStyleDos = {
+    //     display: visible ? 'flex' : 'none',
+    //     opacity: '1',
+    //     transition: 'opacity 1.2s'
+    // }
+
+    // const [date, setDate] = useState();
 
   return (
     <div className='text-white my-[175px]'>
         <div className='sm:flex sm:flex-col hidden items-center text-center sm:mx-auto mx-[30px] mb-[40px] sm:text-lg text-md font-semibold cursor-default'>
             <p>Yoooo! Looks like the terrace is not occupied right now.</p>
-            <p className='flex'>Press the button below to schedule a booking! <span className='ml-1 pt-1 text-xl'><CiBeerMugFull /></span></p>
+            <p className='flex'>Press the button below to schedule a booking! <span className='ml-1 md:pt-1 text-xl'><CiBeerMugFull /></span></p>
         </div>
         <div className='flex flex-col sm:hidden items-center text-center sm:mx-auto mx-[30px] mb-[40px] sm:text-lg text-md font-semibold cursor-default leading-relaxed'>
             <p>Yoooo! Looks like the terrace is not occupied right now. Press the button</p>
             <p className='flex'>below to schedule a booking! <span className='ml-1 pt-1 text-xl'><CiBeerMugFull /></span></p>
         </div>
-        <div className='sm:mx-auto mx-[50px] max-w-[800px] flex justify-center sm:w-full mt-[60px] mb-[0px] transition-all duration-300 items-center sm:space-x-12 space-x-6' onChange={e=>setDate(e.target.value)} style={{display: visible ? 'flex' : 'none'}}>
-            <input type="date" className='text-black md:text-xl text-lg rounded-lg sm:p-4 p-2 transition duration-200 ease-in-out' />
-            <input type="time" className='text-black md:text-xl text-lg rounded-lg sm:p-4 p-2 transition duration-200 ease-in-out' />
-        </div>
 
-        <div className='text-black mx-auto max-w-[500px] flex justify-center w-full mt-[40px] mb-[100px] transition-all duration-300 items-center' style={{display: visible ? 'flex': 'none'}}>
-            <button className='bg-teal-400 sm:p-6 px-8 py-4 text-center rounded-lg flex text-xl font-bold'>
-                BOOK!<GiPartyPopper className='ml-1 mt-1 text-xl'/>
+        {visible &&
+            <div className='sm:mx-auto mx-[50px] max-w-[800px] sm:flex flex-col justify-center sm:w-full mt-[60px] mb-[0px] transition-all duration-300 items-center sm:space-x-0 sm:space-y-8 space-y-6 dateTimeSec'>
+            <div className='sm:block'>
+                <h3 className='sm:text-xl text-lg font-bold text-left mb-4 underline cursor-default'>FROM</h3>
+                <div className='justify-center sm:w-full flex sm:mx-auto space-x-6'>
+                    <input type="date" className='text-black font-medium text-lg rounded-lg p-2 transition duration-200 ease-in-out fromDate' />
+                    <input type="time" className='text-black font-medium text-lg rounded-lg p-2 transition duration-200 ease-in-out fromTime' />
+                </div>
+            </div>
+            <div className='sm:block'>
+                <h3 className='sm:text-xl text-lg font-bold text-left mb-4 underline cursor-default'>TO</h3>
+                <div className='justify-center sm:w-full flex sm:mx-auto space-x-6'>
+                    <input type="date" className='text-black font-medium text-lg rounded-lg p-2 transition duration-200 ease-in-out toDate' />
+                    <input type="time" className='text-black font-medium text-lg rounded-lg p-2 transition duration-200 ease-in-out toTime' />
+                </div>
+            </div>
+            </div>
+        }
+
+        {visible && 
+            <div className='text-black mx-auto max-w-[500px] flex justify-center w-full mt-[50px] mb-[100px] items-center bookBtn'>
+            <button className='bg-teal-400 px-8 py-4 text-center rounded-2xl flex md:text-2xl text-xl font-bold hover:bg-white transition-all duration-300 hover:scale-110 active:bg-white focus:ring-4 focus:ring-teal-400'>
+                BOOK!<GiPartyPopper className='ml-1 mt-1 md:text-2xl text-xl'/>
             </button>
-        </div>
+            </div>
+        }
 
-        <button onClick={() => setVisible(!visible)} className='bg-white backdrop-blur-md bg-opacity-20 flex flex-col px-[50px] max-w-[300px] rounded-2xl items-center transition-color duration-300 ease-in-out shadow-md text-center mx-auto py-6 hover:bg-opacity-50 active:bg-opacity-50 focus:bg-opacity-50 focus:ring focus:ring-teal-400'>
-            <h3 className='text-center md:text-4xl text-2xl font-bold tracking-wide transition-transform duration-300 ease-in-out'>{visible ? 'CANCEL' : 'SCHEDULE'}</h3>
-        </button>
+        {/* {!visible &&
+            <div style={{display: visible ? 'flex' : 'none'}} className='sm:mx-auto mx-[50px] max-w-[800px] sm:flex flex-col justify-center sm:w-full mt-[60px] mb-[0px] transition-all duration-300 items-center sm:space-x-0 sm:space-y-8 space-y-6 notDateTimeSec'>
+            <div className='sm:block'>
+                <h3 className='sm:text-xl text-lg font-bold text-left mb-4 underline cursor-default'>FROM</h3>
+                <div className='justify-center sm:w-full flex sm:mx-auto space-x-6'>
+                    <input type="date" className='text-black font-medium text-lg rounded-lg p-2 transition duration-200 ease-in-out' />
+                    <input type="time" className='text-black font-medium text-lg rounded-lg p-2 transition duration-200 ease-in-out' />
+                </div>
+            </div>
+            <div className='sm:block'>
+                <h3 className='sm:text-xl text-lg font-bold text-left mb-4 underline cursor-default'>TO</h3>
+                <div className='justify-center sm:w-full flex sm:mx-auto space-x-6'>
+                    <input type="date" className='text-black font-medium text-lg rounded-lg p-2 transition duration-200 ease-in-out' />
+                    <input type="time" className='text-black font-medium text-lg rounded-lg p-2 transition duration-200 ease-in-out' />
+                </div>
+            </div>
+            </div>
+        }
+
+        {!visible &&
+            <div className='text-black mx-auto max-w-[500px] flex justify-center w-full mt-[50px] mb-[100px] items-center notBookBtn' style={{display: visible ? 'flex' : 'none'}}>
+            <button className='bg-teal-400 px-8 py-4 text-center rounded-2xl flex md:text-2xl text-xl font-bold hover:bg-white transition-all duration-300 hover:scale-110 active:bg-white focus:ring-4 focus:ring-teal-400'>
+                BOOK!<GiPartyPopper className='ml-1 mt-1 md:text-2xl text-xl'/>
+            </button>
+            </div>
+        } */}
+
+            <button onClick={() => setVisible(!visible)} className='bg-white backdrop-blur-md bg-opacity-20 flex flex-col px-[50px] max-w-[300px] rounded-2xl items-center transition-all duration-300 ease-in-out shadow-md text-center mx-auto py-6 md:hover:scale-110 hover:bg-opacity-50 active:bg-opacity-50 focus:ring focus:ring-teal-400'>
+                <h3 className='text-center md:text-4xl text-2xl font-bold tracking-wide transition-all duration-300 ease-in-out schedBtn'>{visible ? 'CANCEL' : 'SCHEDULE'}</h3>
+            </button>
     </div>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,9 +16,9 @@ export const Footer = () => {
   return (
     <div className='flex flex-col w-full bg-black bg-opacity-30 backdrop-blur-md py-4 text-white'>
         <p className='flex justify-between md:text-md text-sm font-semibold'>
-            <a href='https://www.linkedin.com/in/raajveer-khattar/' target='_blank' rel='noopener noreferrer' className='sm:pl-20 pl-6 pr-8 mt-3 cursor-pointer'><BsLinkedin /></a>
+            <a href='https://www.linkedin.com/in/raajveer-khattar/' target='_blank' rel='noopener noreferrer' className='sm:pl-20 pl-6 pr-8 mt-1 cursor-pointer'><BsLinkedin /></a>
             <span className='text-center cursor-default'>'Who's at the Terrace?' made by <a href='https://linktr.ee/raajveerkhattar' target='_blank' rel='noopener noreferrer' className='underline text-teal-400 cursor-pointer'>Raajveer Khattar</a></span>
-            <a href='https://github.com/raajveerk' target='_blank' rel='noopener noreferrer' className='sm:pr-20 pr-6 pl-8 mt-3 cursor-pointer'><BsGithub /></a>
+            <a href='https://github.com/raajveerk' target='_blank' rel='noopener noreferrer' className='sm:pr-20 pr-6 pl-8 mt-1 cursor-pointer'><BsGithub /></a>
         </p>
     </div>
   )
